6183f6a3051be2cf77702524404cc2978356bfe2
sahil <sahils.verma.1000@gmail.com>
2025-08-07 13:53:26 +0530

    config mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import * as wsApi from "./socket/index.js";
 import * as restApi from "./rest.js";
+import connectDB from "./database/connection.js";
 
 async function bootstrap() {
   try {
-    const port = 8000;
-    const httpServer = restApi.listen(port);
+    const PORT = 8000;
+    const DATABASE_NAME = "collabhub";
+    
+    await connectDB(DATABASE_NAME);
+
+    const httpServer = restApi.listen(PORT);
+
     wsApi.listen(httpServer);
   } catch (err) {
     console.log(chalk.red("App Bootstrap Error: ", err));
