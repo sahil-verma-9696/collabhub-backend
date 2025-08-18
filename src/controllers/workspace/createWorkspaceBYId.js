0a4a10f91a98a4fb5a8bedeed18193f223cb6cf1
unknown <sonalv308@gmail.com>
2025-08-18 11:26:18 +0530

    fix name issue of createWorkspace

--- src/controllers/workspace/createWorkspaceBYId.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import Workspace from '../../models/workspace.model';
-async function createWorkspaceById(req,res){
-   const{name,description,members} = req.body
-
-  const workspace =  await Workspace.create({})
-}
-
-export default createWorkspaceById;
--- src/controllers/workspace/createWorkspaceById.js
+++ src/controllers/workspace/createWorkspaceById.js
@@ -1,9 +1,21 @@
-import mongoose from 'mongoose';
-import Workspace from '../../models/workspace.model';
-async function createWorkspaceById(req,res){
-   const{name,description,members} = req.body
+import Workspace from "../../models/workspace.model.js";
+async function createWorkspaceById(req, res) {
+  const { name, des } = req.body;
 
-  const workspace =  await Workspace.create({})
+  if (!name && !description) {
+    const err = new Error("name and password is required");
+    err.status = 401;
+    throw err;
+  }
+
+  const workspace = await Workspace.create({
+    name,
+    description: des,
+    owner: req.user._id,
+  });
+
+  await workspace.save();
+  res.status(201).json({message:"workspace created successfully",data:workspace})
 }
 
 export default createWorkspaceById;
