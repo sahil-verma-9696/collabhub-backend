ff7a831268ac22f837906327c7062eeac9910a3c
unknown <sonalv308@gmail.com>
2025-08-18 11:22:17 +0530

    working workspace controller

--- src/controllers/workspace/createWorkspaceById.js
+++ src/controllers/workspace/createWorkspaceById.js
@@ -1,21 +1,9 @@
-import Workspace from "../../models/workspace.model.js";
-async function createWorkspaceById(req, res) {
-  const { name, des } = req.body;
+import mongoose from 'mongoose';
+import Workspace from '../../models/workspace.model';
+async function createWorkspaceById(req,res){
+   const{name,description,members} = req.body
 
-  if (!name && !description) {
-    const err = new Error("name and password is required");
-    err.status = 401;
-    throw err;
-  }
-
-  const workspace = await Workspace.create({
-    name,
-    description: des,
-    owner: req.user._id,
-  });
-
-  await workspace.save();
-  res.status(201).json({message:"workspace created successfully",data:workspace})
+  const workspace =  await Workspace.create({})
 }
 
 export default createWorkspaceById;
