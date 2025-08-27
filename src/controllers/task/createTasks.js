ac6552e078403df5ca0a015ec20d3aa2ee5b07cf
Vansh nigam <vanshnigam8081432513@gmail.com>
2025-08-27 15:05:23 +0530

    task route complete

--- src/controllers/task/createTasks.js
+++ src/controllers/task/createTasks.js
@@ -1,12 +1,11 @@
 import Task from '../../models/task.model.js';
 
+const createTasks = async (req, res) => {
 
-const createTaskById = async(req , res)=>{
-    
-    const {title , description ,status , label , date} = req.body;
-    const {module_id } = req.params;
+    const { title, description, status, label, date } = req.body;
+    const { module_id } = req.params;
 
-    if(!title){
+    if (!title ) {
         const err = new Error("Title is required");
         err.statusCode = 401;
         throw err;
@@ -18,13 +17,11 @@ const createTaskById = async(req , res)=>{
         status,
         label,
         date,
-        module:module_id
+        module: module_id
     })
 
     await newTask.save();
-    res.status(201).json({message:"Your Task is Created Succesfully", data :newTask});
-
-
+    res.status(201).json({ message: "Your Task is Created Succesfully", data: newTask });
 }
 
-export default createTaskById;
+export default createTasks;
